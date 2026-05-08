1c332a704cdf8c44d5ae50844cdf65deb14ef637
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2026-05-08 12:52:52 +0300

    go react

--- src/components/CafeStatistics/CafeStatistics.jsx
+++ src/components/CafeStatistics/CafeStatistics.jsx
@@ -4,9 +4,9 @@ import { StatisticsComp, ButtonBlock } from './CafeStatistics.styled';
 
 export class CafeStatistics extends Component{
     state = {
-            good:    0,
-            neutral: 0,
-            bad:     0,
+            good:   0,
+            neutral:0,
+            bad:    0,
             }
     
    handleBtn = e => {
@@ -18,10 +18,25 @@ export class CafeStatistics extends Component{
       };
     });
   };
+
+ 
+
+  countTotalFeedback = () =>{
+      return Object.values(this.state).reduce((acc, itm)=>(acc += itm), 0);
+    }
+  countPositiveFeedbackPercentage = () => {
+    const res = ((this.state.good / this.countTotalFeedback())  * 100)
+    return res ? res : 0; 
+     
+  }
   
            
     render(){
-        const{ good, neutral, bad } = this.state;  
+        const{ good, neutral, bad } = this.state; 
+        const total =  this.countTotalFeedback();
+        let persent = this.countPositiveFeedbackPercentage().toFixed();
+       console.log(!persent);
+       
         return (
             <StatisticsComp>
                 <h2>Please leave feedback</h2>
@@ -31,9 +46,11 @@ export class CafeStatistics extends Component{
                 <button type="button" onClick={this.handleBtn}>Bad</button>
                 </ButtonBlock>
                 <h3>Statistics</h3>
-                <p>Good:    {good}</p>
-                <p>Neutral: {neutral}</p>
-                <p>Bad:     {bad}</p>
+                <p>Good: <span>{good}                </span></p>
+                <p>Neutral: <span>{neutral}          </span></p>
+                <p>Bad: <span>{bad}                  </span></p>
+                <p>Total: <span>{total}              </span></p>
+                <p>Positive feedback: <span>{ persent }</span>%</p>
             </StatisticsComp>
             
         )
--- src/components/CafeStatistics/CafeStatistics.styled.jsx
+++ src/components/CafeStatistics/CafeStatistics.styled.jsx
@@ -4,15 +4,14 @@ export const ButtonBlock = styled.div `
     button{ 
     color: #fff;
     font-size: 1em;
-    padding: 7px 12px;
+    padding: 7px 17px;
     background-color: rgb(14, 142, 255);
     border-radius: 4px;
     border: none;
     }
     button:not(:first-child) {
     margin-left: 8px;
-    background-color: #893131;
-  }
+    }
 `;
 
 export const StatisticsComp = styled.div`
@@ -20,8 +19,12 @@ export const StatisticsComp = styled.div`
    border: 1px solid black;
 
    p{
-    color: red;
+    color: #000000;
     font-size: 1.2em;
+   }
+
+   p span{
+    font-weight: bold;
    }
   
 `;
